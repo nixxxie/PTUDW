55d82d7118cf647ca5baa37d0d1d08cbcbc5648b
nguyendinhit <48084269+nguyendinhinit@users.noreply.github.com>
2019-12-11 08:14:03 +0700

    fix teacher

--- teacher/modules/header_teacher.php
+++ teacher/modules/header_teacher.php
@@ -1,15 +1,20 @@
-<?php session_start();?>
-<div class="w3-green w3-container w3-twothird">
-          <h2></h2>
-        </div>
-        <div class="w3-container w3-third">
-            <div class="w3-row">
-                <div class="w3-half w3-container w3-green">
-                  <h2> <a href="layout_teacher.php?xem=teacher_profile"><?php echo $_SESSION['teacher_name']?></a></h2>
-                </div>
-                <div class="w3-half w3-container">
-                <h2><a href = "../login/login_verify/logout.php">Sign Out</a></h2>
-                </div>
-              </div>
-              
-        </div>
+<?php session_start(); ?>
+<style>
+  #signout:hover {
+    color: red;
+  }
+</style>
+<div class="w3-light-green w3-container w3-twothird">
+  <h2></h2>
+</div>
+<div class="w3-container w3-third">
+  <div class="w3-row">
+    <div class="w3-half w3-container w3-light-green">
+      <h3> <a href="layout_teacher.php?xem=teacher_profile" style="text-decoration: none"><?php echo $_SESSION['teacher_name'] ?></a></h3>
+    </div>
+    <div class="w3-half w3-container">
+      <h3><a href="../login/login_verify/logout.php" style="text-decoration: none" id=signout>Sign Out</a></h3>
+    </div>
+  </div>
+
+</div>
